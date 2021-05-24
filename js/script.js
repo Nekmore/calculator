afb07db0bbc982cf6ffd3f5160a8247907b63d8b
Nekmore <nekmorez@gmail.com>
2021-05-24 23:43:05 +0700

    calculator

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ window.onload = function () {
       },
       rublesInc: function() {
         this.incRubles = rubles(this.inc)
-        let copRubles = this.incRubles.split(' ').pop()
+        copRubles = this.incRubles.split(' ').pop()
         if (this.inc == 0) {
           this.incRubles = "ноль рублей ноль копеек"
         } else if (this.pennyI() == 0) {
@@ -164,35 +164,3 @@ window.onload = function () {
     }
   })
 }
-
-
-var VueScrollTo = require('vue-scrollto');
- 
-var options = {
-    container: '#container',
-    easing: 'ease-in',
-    lazy: false,
-    offset: -60,
-    force: true,
-    cancelable: true,
-    onStart: function(element) {
-      // scrolling started
-    },
-    onDone: function(element) {
-      // scrolling is done
-    },
-    onCancel: function() {
-      // scrolling has been interrupted
-    },
-    x: false,
-    y: true
-}
- 
-// var cancelScroll = VueScrollTo.scrollTo(element, duration, options)
- 
-// or alternatively inside your components you can use
-let cancelScroll = this.$scrollTo(element, duration, options)
- 
-// to cancel scrolling you can call the returned function
-cancelScroll()
-
